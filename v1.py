57d0a2bff757a3b1625f533f2ba9b8ea0680ae55
Patricio Miranda <55902337+PatMir@users.noreply.github.com>
2023-10-04 21:30:50 -0300

    Archivo interfaz
    Usa PLN como biblioteca, se necesita instalar la biblioteca de dearpygui

---
+++ v1.py
@@ -0,0 +1,43 @@
+import dearpygui.dearpygui as dpg
+import PLN  
+
+dpg.create_context()
+dpg.create_viewport(title='Custom Title', width=1280, height=720)
+dpg.setup_dearpygui()
+
+fullscreen_bool = 0
+
+def analisis():
+    frase_ingresada = dpg.get_value("string_value")
+    print(PLN.analisis_texto(frase_ingresada))
+
+def change_to_fullscreen():
+    dpg.toggle_viewport_fullscreen()
+    global fullscreen_bool
+    fullscreen_bool = 1
+
+def change_resolution(width_int, height_int):
+    dpg.configure_viewport('Custom Title', width=1280, height=720)
+    global fullscreen_bool
+    if fullscreen_bool == 1:    
+        dpg.toggle_viewport_fullscreen()
+
+
+
+with dpg.viewport_menu_bar():
+    with dpg.menu(label="Configuracion"):
+        with dpg.menu(label="Resolucion"):
+            dpg.add_menu_item(label="Pantalla Completa", callback=lambda:change_to_fullscreen())
+            dpg.add_menu_item(label="1280x720", callback=lambda:change_resolution(1280,720))
+
+    with dpg.window(label="Entrada de texto",autosize = True, no_move = True, no_close = True):
+
+        dpg.add_input_text(label="Ingrese su frase", tag="string_value")
+        dpg.add_button(label="Ok", callback=analisis)
+
+
+
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
+    
