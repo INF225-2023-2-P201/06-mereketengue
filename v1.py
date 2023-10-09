c80c252ca9ddd3c95535b3c1ad0d1db30c02a62c
Patricio Miranda <55902337+PatMir@users.noreply.github.com>
2023-10-08 22:21:30 -0300

    Update v0.1.1.py
    Bug fix

--- v1.py
+++ v1.py
@@ -2,7 +2,7 @@ import dearpygui.dearpygui as dpg
 import PLN  
 
 dpg.create_context()
-dpg.create_viewport(title='Custom Title', width=1280, height=720)
+dpg.create_viewport(title='Interpretador Mereketengue', width=1280, height=720)
 dpg.setup_dearpygui()
 
 fullscreen_bool = 0
@@ -17,10 +17,11 @@ def change_to_fullscreen():
     fullscreen_bool = 1
 
 def change_resolution(width_int, height_int):
-    dpg.configure_viewport('Custom Title', width=1280, height=720)
+    dpg.configure_viewport('Interpretador Mereketengue', width = width_int, height = height_int)
     global fullscreen_bool
     if fullscreen_bool == 1:    
         dpg.toggle_viewport_fullscreen()
+        fullscreen_bool = 0
 
 
 
@@ -29,6 +30,7 @@ with dpg.viewport_menu_bar():
         with dpg.menu(label="Resolucion"):
             dpg.add_menu_item(label="Pantalla Completa", callback=lambda:change_to_fullscreen())
             dpg.add_menu_item(label="1280x720", callback=lambda:change_resolution(1280,720))
+            dpg.add_menu_item(label="1920x1080", callback=lambda:change_resolution(1920,1080))
 
     with dpg.window(label="Entrada de texto",autosize = True, no_move = True, no_close = True):
 
