d3d5dcbcb56e487789ef1ae8a260d49ff6a21a2c
Patricio Miranda <55902337+PatMir@users.noreply.github.com>
2023-11-06 04:16:33 -0300

    Update PLN.py
    Mejoras de comandos y un nuevo comando:
    
    - Se crea una carpeta Historial, donde se guardan las acciones en un .txt ,con formato nombre de la fecha y hora de la acción 
    - Se agregan adverbios de negación reconocibles
    - Se agregan palabras que inicien el comando de bloquear internet
    - Se mejora el código para guardar el nombre del nodo o la ip
    - Se agrega el comando de hacer ping

--- PLN.py
+++ PLN.py
@@ -1,60 +1,122 @@
-import spacy
+import spacy, os, datetime
 
 # Cargar el modelo de lenguaje
 nlp = spacy.load("es_core_news_sm")
 
+# Crea una subCarpeta llamada "Historial" 
+directorio_base = os.getcwd()
 
+if True != os.path.exists('Historial'): # Si la carpeta ya existe no la crea
+    os.mkdir('Historial')
+
+#Funcion que analiza la entrada texto
 def analisis_texto(texto):
     # Procesar el texto con spaCy
     doc = nlp(texto)
-    bloqueo = ["bloquear", "impedir", "prohibir"]
-    acceso = ["conectarse", "acceso", "entrada", "salir"]
+    
+    bloqueo = ["bloquear", "impedir", "prohibir", "cerrar", "cortar", "incomunicar", "aislar"]
+    acceso = ["conectarse", "conexion", "acceso", "entrada", "salir"]
+    ping = ["pingear", "ping", "latencia"]
 
-    # Definir una lista de negaciones en español
-    negaciones = ["no", "nunca", "jamás", "tampoco"]
+    # Definir una lista de adverbios de negaciones en español
 
-    # Variables para controlar si se bloquea o se permite el acceso a internet
+    negaciones = ["no", "nada", "nunca", "jamás", "nadie", "tampoco", "ni"]
+
+    # Variables para controlar los posibles comandos que estan implementados
     bloquear_internet = False
     permitir_internet = False
+    ping_conexion = False
     negacion = False
-    sust= ""#NODO
-    next_sust = ""#A
-    ip=0
+    
+    sust_1 = "" #NODO
+    next_sust_1 = ""#A
+    sust_2 = "" #NODO
+    next_sust_2 = "" #B
+    ip_1 = 0
+    ip_2 = 0
 
     # Recorrer los tokens del texto
     for i, token in enumerate(doc):
 
+        print(token.text, token.pos_)
+        #Guarda Nodo con nombre
+        if token.text.lower() == "nodo":
+            if sust_1 == "":
+                sust_1 = token.text
+                next_sust_1 = doc[i+1].text
+            else:
+                sust_2 = token.text
+                next_sust_2 = doc[i+1].text
+        #Guarda la(s) ip(s)
+        if token.pos_== "NUM" and "." in token.text:
+            if ip_1 == 0:
+                ip_1 = token.text
+            else:
+                ip_2 = token.text
+
         # Verificar si el token es un adverbio de negación
-        print(token,token.pos_)
-        if token.pos_=="NOUN":
-            sust=token.text
-            next_sust= doc[i+1]
-        if token.pos_=="NUM":
-            ip=token.text
-            
         if token.text.lower() in negaciones:
-            # Verificar si el token anterior está en la lista de bloqueo o acceso
             negacion = True
+
+        # Verificar si el token anterior está en la lista de bloqueo
         if token.text.lower() in bloqueo:
             bloquear_internet = True
+        # Verifica si el token esta en la lista de acceso
         elif token.text.lower() in acceso:
             permitir_internet = True
+        # Verifica si el token esta en la lista de Ping
+        elif token.text.lower() in ping:
+            ping_conexion = True
+        #Fin del for
 
     # Determinar el resultado final
     if (bloquear_internet == True and negacion == False) or (permitir_internet == True and negacion == True):
-        print("Se bloquea el acceso a internet.")
-        if sust!="":
-         print("Output: \n$configure terminal\n$access-list extended block-out\n$deny ip",sust,next_sust,"any\n$exit\n$interface eth0\n$ip access-group block-out in\n$exit" )
+        if sust_1 != "":
+            #COMPROBAR SI LA IP ESTA EN LA BD
+            print("Se bloquea el acceso a internet.")
+            fecha = datetime.datetime.now()
+            archi = open('Historial/{0}.txt'.format(fecha.strftime("%d-%m-%Y_%H-%M-%S")), 'wt')
+            #BUSCAR LA IP DE SUST NEXT_SUST EN LA BASE DE DATOS, TODAVIA NO LO APLICO
+            text = str("Output: \n$configure terminal\n$access-list extended block-out\n$deny ip" + sust_1 + " " + next_sust_1 + " any\n$exit\n$interface eth0\n$ip access-group block-out in\n$exit")
+            archi.write(text)
+            archi.close()
+        elif ip_1 != 0:
+            #COMPROBAR SI LA IP ESTA EN LA BD
+            print("Se bloquea el acceso a internet.")
+            fecha = datetime.datetime.now()
+            archi = open('Historial/{0}.txt'.format(fecha.strftime("%d-%m-%Y_%H-%M-%S")), 'wt')
+            text = str("Output: \n$configure terminal\n$access-list extended block-out\n$deny ip " + ip_1 + " any\n$exit\n$interface eth0\n$ip access-group block-out in\n$exit")
+            archi.write(text)
+            archi.close()
         else:
-           print("Output: \n$configure terminal\n$access-list extended block-out\n$deny ip",ip,"any\n$exit\n$interface eth0\n$ip access-group block-out in\n$exit" )  
+            print("Especifique un objeto valido para bloquear internet")
     elif (bloquear_internet == True and negacion == True) or (permitir_internet == True and negacion == False):
+        #AGREGAR EL CODIGO NECESARIO
         print("Se permite el acceso a internet.")
-
+    elif (ping_conexion == True):
+        #AGREGAR CODIGO NECESARIO EN TODOS LOS IF/ELIF
+        if sust_1 != "":
+            if sust_2 != "":
+                print("Hacer ping entre {0} {1} y {2} {3}".format(sust_1, next_sust_1, sust_2, next_sust_2))
+            elif ip_1 != 0:
+                print("Hacer ping entre {0} {1} y {2}".format(sust_1, next_sust_1, ip_1))
+            else:
+                print("Especifica una segunda conexion")
+        elif ip_1 != 0:
+            if sust_1 != "":
+                print("Hacer ping entre {0} y {1} {2}".format(ip_1, sust_1, next_sust_1))
+            elif ip_2 != 0:
+                print("Hacer ping entre {0} y {1}".format(ip_1, ip_2))
+            else:
+                print("Especifica una segunda conexion")
+        else:
+            print("Especifica una conexion")
+    #FALTAN MAS COMANDOS??
     else:
-        print("No se especifica el acceso a internet.")
+        print("Ningun comando conocido.")
 
 # Texto en lenguaje natural
 
 # Bloquear internet para el nodo A
-# print(analisis_texto("El 192.125.000 no puede salir a internet"))
-# texto = "El 192.125.000 no puede salir a internet"
+#print(analisis_texto("Quiero hacer ping entre el nodo A y 192.125.001"))
+#texto = "El 192.125.000 no puede salir a internet"
